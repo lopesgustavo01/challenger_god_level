e26dd4334d27b3af3cddfb6de2a0bdcbf327f50e
Luiz Gustavo S Lopes <97141086+lopesgustavo01@users.noreply.github.com>
2025-11-02 14:06:36 -0300

    aa

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,3 +6,10 @@ dotenv.config();
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
+
+pool.on('connect', (client) => {
+  client.query('SET search_path TO public')
+    .catch((err) => {
+      console.error('Erro ao configurar search_path:', err);
+    });
+});
